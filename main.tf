ef312db2924e992688f6b69013717504d967192b
Michael Wittig <michael@widdix.de>
2023-04-28 10:59:37 +0200

    feat: add module_version_monitoring_enabled var

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ JSON
 
 resource "aws_cloudwatch_event_rule" "monitoring_jump_start_connection" {
   depends_on = [aws_sns_topic_subscription.marbot]
-  count      = var.enabled ? 1 : 0
+  count      = (var.module_version_monitoring_enabled && var.enabled) ? 1 : 0
 
   name                = "marbot-alb-connection-${random_id.id8.hex}"
   description         = "Monitoring Jump Start connection. (created by marbot)"
@@ -105,7 +105,7 @@ resource "aws_cloudwatch_event_rule" "monitoring_jump_start_connection" {
 }
 
 resource "aws_cloudwatch_event_target" "monitoring_jump_start_connection" {
-  count = var.enabled ? 1 : 0
+  count = (var.module_version_monitoring_enabled && var.enabled) ? 1 : 0
 
   rule      = join("", aws_cloudwatch_event_rule.monitoring_jump_start_connection.*.name)
   target_id = "marbot"
@@ -114,7 +114,7 @@ resource "aws_cloudwatch_event_target" "monitoring_jump_start_connection" {
 {
   "Type": "monitoring-jump-start-tf-connection",
   "Module": "alb",
-  "Version": "1.1.0",
+  "Version": "1.2.0",
   "Partition": "${data.aws_partition.current.partition}",
   "AccountId": "${data.aws_caller_identity.current.account_id}",
   "Region": "${data.aws_region.current.name}"
